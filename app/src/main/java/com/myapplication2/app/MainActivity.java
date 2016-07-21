c05edf681b97ad5f5f29dc6a8bdb4dfe267d54c8
jagtejsodhi <jagtej.sodhi@gmail.com>
2016-07-20 23:45:38 -0700

    sample

---
+++ app/src/main/java/com/myapplication2/app/MainActivity.java
@@ -0,0 +1,120 @@
+package com.myapplication2.app;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import com.google.vr.sdk.widgets.video.VrVideoEventListener;
+import com.google.vr.sdk.widgets.video.VrVideoView;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected VrVideoView videoWidgetView;
+    private VideoLoaderTask backgroundVideoLoaderTask;
+
+    /** Tracks the file to be loaded across the lifetime of this app. **/
+    private Uri fileUri;
+
+    /** Configuration information for the video. **/
+    private VrVideoView.Options videoOptions = new VrVideoView.Options();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoWidgetView = (VrVideoView) findViewById(R.id.video_view);
+        videoWidgetView.setEventListener(new ActivityEventListener());
+
+        backgroundVideoLoaderTask = new VideoLoaderTask();
+        backgroundVideoLoaderTask.execute(Pair.create(fileUri, videoOptions));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class ActivityEventListener extends VrVideoEventListener {
+        /**
+         * Called by video widget on the UI thread when it's done loading the video.
+         */
+        @Override
+        public void onLoadSuccess() {
+        }
+
+        /**
+         * Called by video widget on the UI thread on any asynchronous error.
+         */
+        @Override
+        public void onLoadError(String errorMessage) {
+            // An error here is normally due to being unable to decode the video format.
+        }
+
+        @Override
+        public void onClick() {
+        }
+
+        /**
+         * Update the UI every frame.
+         */
+        @Override
+        public void onNewFrame() {
+        }
+
+        /**
+         * Make the video play in a loop. This method could also be used to move to the next video in
+         * a playlist.
+         */
+        @Override
+        public void onCompletion() {
+            videoWidgetView.seekTo(0);
+        }
+    }
+
+    class VideoLoaderTask extends AsyncTask<Pair<Uri, VrVideoView.Options>, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Pair<Uri, VrVideoView.Options>... fileInformation) {
+            try {
+                if (fileInformation == null || fileInformation.length < 1
+                        || fileInformation[0] == null || fileInformation[0].first == null) {
+                    // No intent was specified, so we default to playing the local stereo-over-under video.
+                    VrVideoView.Options options = new VrVideoView.Options();
+                    options.inputType = VrVideoView.Options.TYPE_STEREO_OVER_UNDER;
+                    videoWidgetView.loadVideoFromAsset("congo.mp4", options);
+                } else {
+                    videoWidgetView.loadVideo(fileInformation[0].first, fileInformation[0].second);
+                }
+            } catch (IOException e) {
+                // An error here is normally due to being unable to locate the file.
+                Log.e("Jagtej", "Could not open video: " + e);
+            }
+
+            return true;
+        }
+    }
+}
